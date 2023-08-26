704189df28faf10f3a579acca13a0173e93186c9
Sascha Block <Block@inztitut.de>
2023-08-26 10:11:43 +0200

    Implementing Vuetify and components Login.vue, MyComponent.vue and Profile.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt3';
 
 export default defineNuxtConfig({
   vue: {
